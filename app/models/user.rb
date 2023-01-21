491b815a1b7c94e9e7f5f1f0654343bcfadfab06
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-22 00:15:07 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true       
-         
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字（漢字、ひらがな、カタカナ）を使用してください' } do
@@ -19,6 +19,5 @@ class User < ApplicationRecord
     validates :first_name_kana
   end
 
-  validates :birthday, presence: true 
-            
+  validates :birthday, presence: true
 end
