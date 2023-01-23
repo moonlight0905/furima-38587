2e320a329a01b51dabfcf6d206fd7d69fb64e30e
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-23 22:01:32 +0900

    itemsコントローラー#new,#create追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,22 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+     redirect_to root_path
+    else
+     render "new"
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :text, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
   end
+
 end
