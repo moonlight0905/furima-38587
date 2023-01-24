4fbb6ec4761f4de08e49b3895a5b39a750d4e84f
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-24 22:28:45 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,28 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id
-  belongs_to :condition_id
-  belongs_to :shipping_cost_id
-  belongs_to :prefecture_id
-  belongs_to :days_to_ship_id
-
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_to_ship
 
   validates :item_name,         presence: true
   validates :text,              presence: true
   validates :image,             presence: true
+
   has_one_attached :image
+
   with_options presence: true, format: { with: /\A[0-9]+\z/} do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,
                                       message: 'は¥300 ~ ¥9,999,999の半角数値で入力してください' } 
   end
-  validates :category_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :condition_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :shipping_cost_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :prefecture_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :days_to_ship_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :category_id,       presence: true
+  validates :condition_id,      presence: true
+  validates :shipping_cost_id,  presence: true
+  validates :prefecture_id,     presence: true
+  validates :days_to_ship_id,   presence: true
+
+  belongs_to :user
 
 end
