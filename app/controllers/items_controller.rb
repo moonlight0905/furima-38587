e3940579ad492c0800aca727114d2e1f55a0062f
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-27 22:32:43 +0900

    showアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :text, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
-                                 :days_to_ship_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :text, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
   end
 end
