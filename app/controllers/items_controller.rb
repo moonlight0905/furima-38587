c1e97f7f6e2872fd5fc336e51429a4bdbb02149c
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-27 22:38:51 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :text, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :text, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
+                                 :days_to_ship_id, :image).merge(user_id: current_user.id)
   end
 end
