59367166795da602a0a438d13b9483a5a79002a9
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-28 20:57:11 +0900

    #edit#update定義とバリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :correct_item, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,6 +21,27 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def correct_item
+    @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to root_path
+    end 
   end
 
   private
