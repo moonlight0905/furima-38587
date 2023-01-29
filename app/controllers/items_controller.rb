fc8a12beab7c41779a463cdd2c3c4be28965ee68
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-29 20:48:51 +0900

    #destroy修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if user_signed_in? && @item.user == current_user
+      @item.destroy
       redirect_to root_path
     end
   end
