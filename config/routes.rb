3f91f6b807c3e4d75417a7c68cb35d4e15d2f3b3
moonlight0905 <harvest.moon.0905@gmail.com>
2023-01-31 20:03:19 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
  devise_for :users
  root to: 'items#index'
- resources :items
- resources :purchases, only: :index
+ resources :items do
+  resources :purchases, only: :index do
+    resources :destination
+  end 
+ end
 end
