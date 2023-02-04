e0f783f367d7ba696095e2ce5e5a8705ef3a5bdb
moonlight0905 <harvest.moon.0905@gmail.com>
2023-02-04 23:00:11 +0900

    purchaseモデルのテストコード実装

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入記録新規登録' do
+    before do
+      @purchase = FactoryBot.build(:purchase)
+    end
+
+    context '内容に問題ない場合' do
+      it 'user,itemが存在すれば登録できる' do
+        expect(@purchase).to be_valid
+      end
+    end
+    context '内容に問題がある場合' do
+      it 'userが紐づいていないと保存できないこと' do
+        @purchase.user = nil
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("User must exist")
+      end
+      it 'itemが紐づいていないと保存できないこと' do
+        @purchase.item = nil
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Item must exist")
+      end
+    end
+  end
 end
