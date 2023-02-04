e7a80713405d0675162df51c877955936e42d2da
moonlight0905 <harvest.moon.0905@gmail.com>
2023-02-04 23:01:23 +0900

    purchase_destinationクラス（フォームオブジェクト）テストコード実装

---
+++ spec/factories/purchase_destinations.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_destination do
+    postal_code { '123-4567' }
+    prefecture_id { 10 }
+    locality { 'テスト市' }
+    address { '1' }
+    building_name { 'テストマンション101' }
+    phone { '09012345678'}
+    purchase_id { 10 } 
+  end
+end
