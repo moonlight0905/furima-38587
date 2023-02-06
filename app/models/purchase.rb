13cecfc6e61ac3c1baa5df592fdeac28be208022
moonlight0905 <harvest.moon.0905@gmail.com>
2023-02-06 21:29:11 +0900

    purchaseモデルのアソシエーション修正

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,4 @@
 class Purchase < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :shipping_cost
-
   belongs_to :user
   belongs_to :item
   has_one    :destination
