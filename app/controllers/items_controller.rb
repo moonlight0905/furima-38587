cf4c53aec79c8020a95b98b90309ac7981d668a5
moonlight0905 <harvest.moon.0905@gmail.com>
2023-05-31 23:03:29 +0900

    ストロングパラメータ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:item_name, :text, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
-                                 :days_to_ship_id, :image).merge(user_id: current_user.id)
+                                 :days_to_ship_id, {images: []}).merge(user_id: current_user.id)
   end
 
   def set_item
