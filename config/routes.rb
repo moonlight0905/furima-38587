de99987c0f9bed0af52160945c3a31cf4f1ad458
moonlight0905 <harvest.moon.0905@gmail.com>
2023-06-16 21:40:13 +0900

    マイページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'cards/new'
  devise_for :users
  root to: 'items#index'
  resources :items do
   resources :purchases, only: [:index, :create]
  end
+ resources :cards, only: [:new, :create]
+ resources :users, only: [:show, :update]
 end
